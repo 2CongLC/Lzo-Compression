94c11e5f3f48e4d3c2f3da5a6b13e34d7a20b2dc
ChuCong <2conglc.vn@gmail.com>
2024-06-26 09:27:06 +0700

    update

---
+++ VBSample/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(83, 202)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(127, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Compress"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(92, 231)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(118, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Decompress"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(22, 28)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(293, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(356, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ VBSample/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports Lzo.net
+Imports Lzo.net.lzoCompressor
+
+Public Class Form1
+
+    Private lz As lzoCompressor
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            OpenFileDialog1.Filter = "All Files|*.*"
+            SaveFileDialog1.Filter = "All Files|*.*"
+
+            If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
+                Dim buffer As Byte() = IO.File.ReadAllBytes(OpenFileDialog1.FileName)
+                lz = New lzoCompressor()
+                Dim data As Byte() = lz.Compress(buffer)
+                File.WriteAllBytes(SaveFileDialog1.FileName, data)
+                MessageBox.Show("Ok")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+End Class
